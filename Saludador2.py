ceda4de8a7dd35b5bd46e13edf5db656f103d83c
Paula Olivan <55576202+PaulaOlivan@users.noreply.github.com>
2019-09-26 09:37:05 +0200

    Add files via upload

---
+++ Saludador2.py
@@ -0,0 +1,6 @@
+def saludador_2():
+    print "Hola amigo"  #Instruccion de salida
+    Nombre= raw_input("Como te llamas?:") #Instruccion de entrada
+    print "Bienvenido a Python " + Nombre
+    
+saludador_2()
