22ce9cea19179cb7aa6b4ffa22739959d610488d
Paula Olivan <55576202+PaulaOlivan@users.noreply.github.com>
2019-10-24 09:17:45 +0200

    Add files via upload

---
+++ Dolares-euro.py
@@ -0,0 +1,7 @@
+#Este programa se encarga de hacer el cambio de dolares a euros
+def dolares_euros():
+    print"Convertidos de dolares a euros"
+    x= input("Cantidad de dolares")
+    print"euros=", x*0.9
+
+dolares_euros()
