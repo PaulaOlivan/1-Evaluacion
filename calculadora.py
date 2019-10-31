e99e5798e04e10a4fb2c26b58aaeb0b870dcd8e4
Paula Olivan <55576202+PaulaOlivan@users.noreply.github.com>
2019-10-31 09:14:38 +0100

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,23 @@
+#este programa recibe dos numeros y se encarga de hacer la operacion deseada
+def calculadora():
+    print "Ha iniciado el programa de la calculadora"
+    x= input ("Cual es el primer numero")
+    y= input ("Cual es el segundo numero")
+    repetir = True
+    while(repetir==True):
+
+        o= raw_input ("Elija la operacion deseada: s para sumas, r para restas, m para multiplicacion y d para division")
+        if(o != "s" or o!= "r" or o!= "m" or o!= "d" ):
+            repetir=False
+        else:
+            repetir=True
+    if(o=="s"):
+        respuesta=x+y
+    if(o=="r"):
+        respuesta=x-y
+    if (o=="m"):
+        respuesta=x*y
+    if (o=="d"):
+        respuesta=x/y
+    print respuesta
+calculadora()
