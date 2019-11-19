3dbe5cf6fc12eb3c6347bee759c1ef50578dc934
Paula Olivan <55576202+PaulaOlivan@users.noreply.github.com>
2019-11-19 12:01:36 +0100

    Add files via upload

---
+++ pajarita tumbada.py
@@ -0,0 +1,21 @@
+#Pajarita tumbada
+def pajarita_tumbada():
+    print "La pajarita sera cuadrada (filas y columnas iguales)"
+    altura = input ("Cual es la altura de su pajarita (numero par)= ")
+    asteriscos1 = '*'
+    espacios = ' '
+    asteriscos2 = '*'
+    for i in range (altura/2):
+        for nasteriscos1 in range (i+1, altura):
+            asteriscos = asteriscos + '*'
+        for nespacios in range (,):
+            espacios = espacios + ' '
+        for nastericos2 in range (i+1, altura):
+            asteriscos = astericos + '*'
+        print asteriscos1 + espacios + astericos2
+        astericos1 = '*'
+        espacios = ' '
+        asteriscos2 = '*'
+        
+pajarita_tumbada()
+    
