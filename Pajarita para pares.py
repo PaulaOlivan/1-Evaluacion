4fd08b92261558f3765a8703b46ea51c4bff75fa
Paula Olivan <55576202+PaulaOlivan@users.noreply.github.com>
2019-11-19 11:47:16 +0100

    Add files via upload

---
+++ Pajarita para pares.py
@@ -0,0 +1,22 @@
+#Programa que hace una pajarita
+def pajarita():
+    altura = input ("Cual es la altura total de tu pajarita (numero par): ")
+    espacios = ' '
+    asteriscos = '*'
+    for i in range (altura/2):
+        for nespacios in range (0,i):
+            espacios = espacios + ' '
+        for nasteriscos in range (0,altura-1-2*i-1):
+            asteriscos = asteriscos + '*'
+        print espacios + asteriscos
+        espacios = ' '
+        asteriscos = '*'
+    for i in range(altura/2):
+        for nespacios in range(1, (altura/2)-i):
+            espacios = espacios + ' '            
+        for nasteriscos in range(1,2*i+1):
+            asteriscos = asteriscos + '*'
+        print espacios + asteriscos
+        espacios = ' '
+        asteriscos = '*'
+pajarita()
